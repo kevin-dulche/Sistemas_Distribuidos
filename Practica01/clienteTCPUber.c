c4e2da4bb6b5134171f26ff10b87a14924fb01ee
Kevin Dulche Laptop GMR Ubuntu <kevdulche@gmail.com>
2024-11-12 10:37:05 -0600

    Practica 1 final

--- Practica01/clienteTCPUber.c
+++ Practica01/clienteTCPUber.c
@@ -9,6 +9,7 @@
 #include <time.h>
 
 char datos_recibidos[1024];
+int placas;
 
 void recepcion_datos(int fd) {
     ssize_t bytes_recibidos = recv(fd, datos_recibidos, sizeof(datos_recibidos) - 1, 0);
@@ -19,6 +20,18 @@ void recepcion_datos(int fd) {
     }
     datos_recibidos[bytes_recibidos] = '\0';
     printf("Datos recibidos: %s\n", datos_recibidos);
+
+    // Buscar el valor de "placas" dentro de los datos recibidos
+    char *placas_pos = strstr(datos_recibidos, "\"placas\":");
+    if (placas_pos != NULL) {
+        // Desplazarse hasta el valor de "placas"
+        placas_pos += strlen("\"placas\":");  // Saltar la clave "placas": a
+
+        // Obtener el valor del número después de "placas":
+        placas = atoi(placas_pos);  // Convertir a entero (asegúrate de que sea un número)
+
+        printf("Placas extraídas: %d\n", placas);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -47,7 +60,7 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
-    int opcion, placas;
+    int opcion;
     do {
         printf("\nSelecciona una opción:\n");
         printf("1. Solicitar viaje\n");
@@ -61,12 +74,31 @@ int main(int argc, char *argv[]) {
             snprintf(request, sizeof(request), "{\"tipo\": \"viaje\"}");
             send(fd, request, strlen(request), 0);
             recepcion_datos(fd);
+            close(fd);
 
             // if (strcmp(datos_recibidos, "{\"tipo\": \"viaje_rechazado\"}") == 0) {
             if (strcmp(datos_recibidos, "{\"error\": \"No hay conductores\"}") == 0) {
                 printf("\nNo hay conductores. Saliendo...\n");
                 close(fd);
                 exit(0);
+            }
+
+            fd = socket(AF_INET, SOCK_STREAM, 0);
+            if (fd == -1) {
+                perror("\nError, no se pudo abrir el socket.\n\n");
+                exit(-1);
+            }
+
+            struct sockaddr_in serv;
+            memset(&serv, 0, sizeof(serv));
+            serv.sin_family = AF_INET;
+            serv.sin_addr.s_addr = inet_addr(argv[1]);
+            serv.sin_port = htons(atoi(argv[2]));
+
+            if (connect(fd, (struct sockaddr *)&serv, sizeof(serv)) < 0) {
+                perror("\nNo se pudo realizar la conexión remota.\n");
+                close(fd);
+                exit(-1);
             }
             
             // Simulación del viaje
